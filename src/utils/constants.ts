578f90a681aea99058275c5a8454df838f626943
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-29 23:39:35 +0500

    Add Constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const USER_STATUS = {
+    ACTIVE: "active",
+    INACTIVE: "inactive",
+};
+
+export const SESSION_STATUS = {
+    ACTIVE: "active",
+    REVOKED: "revoked",
+    EXPIRED: "expired",
+};
