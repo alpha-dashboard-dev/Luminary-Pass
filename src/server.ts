2159fcf82621695f182b60a0a0a260944aec600e
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-29 17:14:32 +0500

    Start server with fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+    return { hello: 'world' }
+})
+
+// Run the server!
+try {
+    await fastify.listen({ port: 5000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
