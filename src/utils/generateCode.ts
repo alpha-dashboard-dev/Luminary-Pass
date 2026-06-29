07561d958366289932c68ff69d7d976c181ae19c
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-06-29 23:27:55 +0500

    updated

---
+++ src/utils/generateCode.ts
@@ -0,0 +1,35 @@
+export async function generateCode(model: any, prefix: string, codeField: string): Promise<string> {
+    const lastRecord = await model.findOne({
+        attributes: [codeField],
+        order: [["id", "DESC"]],
+    });
+
+    let nextNumber = 1;
+
+    if (lastRecord) {
+        const lastCode = lastRecord[codeField];
+        nextNumber = Number(lastCode.replace(prefix, "")) + 1;
+    }
+
+    return `${prefix}${nextNumber.toString().padStart(5, "0")}`;
+}
+
+/*
+const organizationCode = await generateCode(
+    Organization,
+    "ORG",
+    "organization_code"
+);
+
+const businessCode = await generateCode(
+    Business,
+    "BUS",
+    "business_code"
+);
+
+const userCode = await generateCode(
+    User,
+    "USR",
+    "user_code"
+);
+ */
