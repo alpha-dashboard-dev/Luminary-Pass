d91c3952ab67d41c7d88c3c31d12afe994d85d5f
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-07-02 17:03:19 +0500

    Add Location, Venue, & Venue Time Table repository

--- src/database/sequelize/models/index.cjs
+++ src/database/sequelize/models/index.cjs
@@ -8,6 +8,10 @@ const RolePermissions = require('./role.permissions');
 const Organization = require('./organization');
 const Business = require("./business.js");
 const UserSession = require('./session');
+const Venue = require('./venue');
+const Category = require('./category');
+const VenueTimeTable = require('./venue_timetable');
+const Location = require('./location');
 
 
 const initModels = () => {
@@ -21,6 +25,10 @@ const initModels = () => {
         Organization: Organization.initModel(sequelize),
         Business: Business.initModel(sequelize),
         UserSession: UserSession.initModel(sequelize),
+        Venue: Venue.initModel(sequelize),
+        Category: Category.initModel(sequelize),
+        VenueTimeTable: VenueTimeTable.initModel(sequelize),
+        Location: Location.initModel(sequelize),
     };
 
     Object.values(models).forEach((model) => {
