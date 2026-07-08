387b133d19df8a5717d1e29718d778a780accfb1
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-07-08 23:59:12 +0500

    Add new permissions in mediaAttachment routes

--- src/routes/mediaAttachment/attachement.routes.ts
+++ src/routes/mediaAttachment/attachement.routes.ts
@@ -1,86 +1,97 @@
-// import { FastifyInstance } from "fastify";
-// import attachmentController from "../../controllers/mediaAttachment/attachment.controller";
-//
-// import { authenticate } from "../../middleware/authenticate";
-// import { hasPermission } from "../../middleware/hasPermission";
-// import {loadPermissions} from "../../middleware/loadPermissions";
-//
-// export default async function attachmentRoutes(fastify: FastifyInstance) {
-//
-//     // CREATE attachment
-//     fastify.post(
-//         "/create-attachment",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.create")
-//             ]
-//         },
-//         attachmentController.create
-//     );
-//
-//     // GET ALL attachment
-//     fastify.get(
-//         "/get-all-attachments",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.read")
-//             ]
-//         },
-//         attachmentController.getAll
-//     );
-//
-//     // GET attachment BY CODE
-//     fastify.get(
-//         "/get-one-attachment/:attachmentCode",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.read")
-//             ]
-//         },
-//         attachmentController.getByAttachmentCode
-//     );
-//
-//     fastify.get(
-//         "/get-attachment-by-field",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.read")
-//             ]
-//         },
-//         attachmentController.getByField
-//     );
-//
-//     // UPDATE attachment
-//     fastify.put(
-//         "/update-attachment/:attachmentCode",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.update")
-//             ]
-//         },
-//         attachmentController.update
-//     );
-//
-//     // DELETE USER
-//     fastify.delete(
-//         "/delete-attachment/:attachmentCode",
-//         {
-//             preHandler: [
-//                 authenticate,
-//                 loadPermissions,
-//                 hasPermission("attachment.delete")
-//             ]
-//         },
-//         attachmentController.delete
-//     );
-// }
+import { FastifyInstance } from "fastify";
+import attachmentController from "../../controllers/mediaAttachment/attachment.controller";
+
+import { authenticate } from "../../middleware/authenticate";
+import { hasPermission } from "../../middleware/hasPermission";
+
+export default async function attachmentRoutes(fastify: FastifyInstance) {
+
+    // CREATE attachment
+    fastify.post(
+        "/create-attachment",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.create
+    );
+
+    // GET ALL attachment
+    fastify.get(
+        "/get-all-attachments",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.getAll
+    );
+
+    // GET attachment BY CODE
+    fastify.get(
+        "/get-one-attachment/:attachmentCode",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.getByAttachmentCode
+    );
+
+    fastify.get(
+        "/get-attachment-by-field",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.getByField
+    );
+
+    // UPDATE attachment
+    fastify.put(
+        "/update-attachment/:attachmentCode",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.update
+    );
+
+    // DELETE USER
+    fastify.delete(
+        "/delete-attachment/:attachmentCode",
+        {
+            preHandler: [
+                authenticate,
+                hasPermission(
+                    ["PER00057", "PER00058", "PER00059", "PER00060"],
+                    false
+                )
+            ]
+        },
+        attachmentController.delete
+    );
+}
