299667aa399584301ac3e96b476c0531dd53409a
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-07-23 23:56:59 +0500

    add max function

--- src/services/analytics/analyticHelper.ts
+++ src/services/analytics/analyticHelper.ts
@@ -1,41 +1,38 @@
 import { fn, col, literal } from "sequelize";
 
-export default {
+class AnalyticsHelper {
 
     count(column = "*") {
         return fn("COUNT", col(column));
-    },
+    }
 
     countDistinct(column: string) {
         return fn("COUNT", fn("DISTINCT", col(column)));
-    },
+    }
 
     avg(column: string) {
         return fn("AVG", col(column));
-    },
+    }
 
     sum(column: string) {
         return fn("SUM", col(column));
-    },
+    }
 
     max(column: string) {
         return fn("MAX", col(column));
-    },
+    }
 
     min(column: string) {
         return fn("MIN", col(column));
-    },
+    }
 
-    month(column: string) {
-        return fn("DATE_TRUNC", "month", col(column));
-    },
-
-    year(column: string) {
-        return fn("DATE_TRUNC", "year", col(column));
-    },
+    dateTrunc(period: "day" | "week" | "month" | "year", column: string) {
+        return fn("DATE_TRUNC", period, col(column));
+    }
 
     literal(sql: string) {
         return literal(sql);
     }
-
 }
+
+export default new AnalyticsHelper();
