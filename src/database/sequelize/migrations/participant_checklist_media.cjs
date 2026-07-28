a9c0e5992f126a825b8db12dd90d5594e6605f60
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-07-28 15:45:53 +0500

    Add participantchecklist Media Table

---
+++ src/database/sequelize/migrations/participant_checklist_media.cjs
@@ -0,0 +1,59 @@
+"use strict";
+
+const {DataTypes} = require("sequelize");
+/** @type {import("sequelize-cli").Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("participant_checklist_media", {
+            id: {
+                type: Sequelize.BIGINT,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+
+            participant_checklist_media_code:{
+                type: Sequelize.STRING(8),
+                unique:true,
+                allowNull:false
+            },
+
+            participant_checklist_code: {
+                type: Sequelize.STRING(8),
+                allowNull: false,
+                unique: true,
+            },
+
+            instagram_media_id:{
+                type: Sequelize.STRING(50),
+                allowNull:true
+            },
+            remarks:{
+                type: Sequelize.STRING(50),
+                allowNull: true,
+            },
+            remarks_by:{
+                type: Sequelize.STRING(8),
+                allowNull: true
+            },
+            status: {
+                type: Sequelize.STRING(100),
+                allowNull: true
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.fn("NOW"),
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.fn("NOW"),
+            },
+        });
+    },
+
+    async down(queryInterface) {
+        await queryInterface.dropTable("participant_checklist_media");
+    },
+};
