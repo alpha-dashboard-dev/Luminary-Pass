93ced7618fec2e560d9f2565971bef020d82a70d
alpha-dashboard-dev <alpha.dashboard.dev@gmail.com>
2026-07-29 00:04:12 +0500

    updated

--- src/services/event/invitation.service.ts
+++ src/services/event/invitation.service.ts
@@ -62,6 +62,8 @@ class EventInvitationService {
 
         const invitationCode = generateCode();
 
+        // add separate column for start & end date and time
+
         const invitation = await eventInvitationRepo.create({
             invitation_code: invitationCode,
             event_code: data.eventCode,
